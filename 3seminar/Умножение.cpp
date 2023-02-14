bcc4d0b60e6c1de8dcbd82a3041a032071f7cd3e
Alisheks <101113879+Alisheks@users.noreply.github.com>
2023-02-14 23:28:59 +0300

    Add files via upload

---
+++ 3seminar/Умножение.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+string operator*(string s, int n) {
+    string output = "";
+    while (n--) {
+        output += s;
+    }
+    return output;
+}
+
+
+int main() {
+    string word{};
+    int n;
+
+    cout << "Enter word:\t";
+    cin >> word;
+
+    cout << "Etner n:\t";
+    cin >> n;
+
+    cout << (word * n);
+    return 0;
+}
