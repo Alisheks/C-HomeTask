404400bb584d4942ce8eba01591363ca27f33016
Alisheks <101113879+Alisheks@users.noreply.github.com>
2023-02-14 23:24:35 +0300

    Add files via upload

---
+++ 1. Измени регистр первой буквы.cpp
@@ -0,0 +1,26 @@
+﻿#include<iostream>
+#include <string>
+using namespace std;
+
+void main()
+{
+    string word;
+
+    cout << "Enter word:\t";
+    cin >> word;
+    int len = word.size();
+
+    if (len == 0) {
+        cout << "";
+        exit()
+    }
+
+    if ((int)word[0] < 95)
+        word[0] = (int)word[0] + 32;
+    else
+        word[0] = (int)word[0] - 32;
+
+    cout << word << endl;
+
+    return 0;
+}
