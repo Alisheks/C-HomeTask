26b73f9f70a8ee6e77109bec56ac6132e066f76c
Alisheks <101113879+Alisheks@users.noreply.github.com>
2023-02-16 16:46:18 +0300

    Add files via upload

---
+++ 3seminar/3_4. Усиление.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+void truncateToDot(std::string& s)
+{
+	int n;
+	n = s.find(".");
+
+	s.erase(n);
+
+	if ((int)s[0] == 34) {
+		s.push_back((char)34);
+	};
+
+	return;
+}
+
+
+int main()
+{
+	string s{};
+
+	cin >> s;
+
+	truncateToDot(s);
+
+	cout << s << "\n";
+	return 0;
+}
