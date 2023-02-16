9a954abee0da31fe3ed4a744cde9b64e9bff59fd
Alisheks <101113879+Alisheks@users.noreply.github.com>
2023-02-16 21:49:15 +0300

    Add files via upload

---
+++ 3seminar/3_5.Сумма из строки.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    // Тут не учитываются отрицательные числа, но в целом, в коде просто появятся пару лишних строк для этого
+
+    string str{}, p = "";
+    int sum = 0;
+
+    cin >> str;
+
+    for (int i = 0; i < str.size(); i++) {
+
+        if ((47 < (int)str[i]) && ((int)str[i] < 58)) {
+            p.push_back(str[i]);
+        }
+
+        else {
+            sum += atoi(p.c_str());;
+            p = "";
+
+        };
+
+    };
+
+    cout << sum;
+
+    return 0;
+}
+
+
