29b125c704b0ca20406aea0a588e94232c65e074
Alisheks <101113879+Alisheks@users.noreply.github.com>
2023-02-24 02:15:33 +0300

    Add files via upload

---
+++ 4seminar/4_3.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <cmath>
+#include <utility>
+
+std::vector<std::pair<int, int>> factorization(int n) {
+    std::vector<std::pair<int, int>> factors;
+    int cnt = 0;
+    for (int i = 2; i <= std::sqrt(n); ++i) {
+        cnt = 0;
+        while (n % i == 0) {
+            ++cnt;
+            n /= i;
+        }
+        if (cnt > 0) {
+            factors.emplace_back(i, cnt);
+        }
+    }
+    if (n > 1) {
+        factors.emplace_back(n, 1);
+    }
+    return factors;
+}
+
+int main() {
+    int n;
+    std::cin >> n;
+    std::vector<std::pair<int, int>> factors = factorization(n);
+    for (auto& factor : factors) {
+        std::cout << factor.first << "^" << factor.second << " ";
+    }
+    std::cout << std::endl;
+    return 0;
+}
